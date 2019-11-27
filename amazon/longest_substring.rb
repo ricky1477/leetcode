c851bcb6b9e32ad69a3fccc193531b07eb81ba7f
Ricardo Kreyhsig <rickreyhsig@gmail.com>
2019-11-27 00:01:53 -0500

    two_sum and longest_substring

---
+++ amazon/longest_substring.rb
@@ -0,0 +1,126 @@
+require "test/unit"
+
+# https://leetcode.com/explore/interview/card/amazon/76/array-and-strings/508/
+
+# Longest Substring Without Repeating Characters
+
+# Given a string, find the length of the longest substring without repeating characters.
+
+# Example 1:
+
+# Input: "abcabcbb"
+# Output: 3 
+# Explanation: The answer is "abc", with the length of 3. 
+# Example 2:
+
+# Input: "bbbbb"
+# Output: 1
+# Explanation: The answer is "b", with the length of 1.
+# Example 3:
+
+# Input: "pwwkew"
+# Output: 3
+# Explanation: The answer is "wke", with the length of 3. 
+#              Note that the answer must be a substring, "pwke" is a subsequence and not a substring.
+
+def longest_substring(s)
+  return 0 if s.empty?
+  substrings = Array.new
+  #sbstr = ''
+  s.each_char.with_index do |char, j|
+    sbstr = ''
+    inner_str = s[j..s.length-1]
+    #p "--#{inner_str}--"
+    inner_str.each_char.with_index do |ch, i|
+      unless (sbstr.include?(ch))
+        sbstr << ch
+        #p "#{sbstr}|#{ch}"
+        # If also last char of input s 
+        substrings << sbstr if (i == inner_str.length - 1)
+      else
+        substrings << sbstr
+        sbstr = ch
+      end
+    #p sbstr
+    end
+  end
+  puts
+  puts "#{s} | #{substrings.inspect}"
+  return substrings.max_by(&:length).length
+end
+
+def longest_substring(s)
+  return s.size if s.size < 2
+      
+  queue, hash = [], {}
+  s.each_char.reduce(-Float::INFINITY) do |max, c|
+    hash.delete(char = queue.shift) until queue.empty? || char.eql?(c) if hash.key?(c)
+    [max, (queue << hash[c] = c).size].max
+  end
+end
+
+# longest_substring('abcabcbb')
+# longest_substring('bbbbb')
+# longest_substring('pwwkew')
+$rly_long_str = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!\"#$%&'()*+,-./:;<=>?@[\\]^_`{|}~
+abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!\"#$%&'()*+,-./:;<=>?@[\\]^_`{|}~ 
+abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!\"#$%&'()*+,-./:;<=>?@[\\]^_`{|}~ 
+abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!\"#$%&'()*+,-./:;<=>?@[\\]^_`{|}~ 
+abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!\"#$%&'()*+,-./:;<=>?@[\\]^_`{|}~ 
+abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!\"#$%&'()*+,-./:;<=>?@[\\]^_`{|}~ 
+abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!\"#$%&'()*+,-./:;<=>?@[\\]^_`{|}~ 
+abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!\"#$%&'()*+,-./:;<=>?@[\\]^_`{|}~ 
+abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!\"#$%&'()*+,-./:;<=>?@[\\]^_`{|}~ 
+abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!\"#$%&'()*+,-./:;<=>?@[\\]^_`{|}~ 
+abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!\"#$%&'()*+,-./:;<=>?@[\\]^_`{|}~ 
+abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!\"#$%&'()*+,-./:;<=>?@[\\]^_`{|}~ 
+abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!\"#$%&'()*+,-./:;<=>?@[\\]^_`{|}~ 
+abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!\"#$%&'()*+,-./:;<=>?@[\\]^_`{|}~ 
+abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!\"#$%&'()*+,-./:;<=>?@[\\]^_`{|}~ 
+abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!\"#$%&'()*+,-./:;<=>?@[\\]^_`{|}~ 
+abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!\"#$%&'()*+,-./:;<=>?@[\\]^_`{|}~ 
+abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!\"#$%&'()*+,-./:;<=>?@[\\]^_`{|}~ 
+abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!\"#$%&'()*+,-./:;<=>?@[\\]^_`{|}~ 
+abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!\"#$%&'()*+,-./:;<=>?@[\\]^_`{|}~ 
+abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!\"#$%&'()*+,-./:;<=>?@[\\]^_`{|}~ 
+abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!\"#$%&'()*+,-./:;<=>?@[\\]^_`{|}~ 
+abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!\"#$%&'()*+,-./:;<=>?@[\\]^_`{|}~ 
+abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!\"#$%&'()*+,-./:;<=>?@[\\]^_`{|}~ 
+abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!\"#$%&'()*+,-./:;<=>?@[\\]^_`{|}~ 
+abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!\"#$%&'()*+,-./:;<=>?@[\\]^_`{|}~ 
+abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!\"#$%&'()*+,-./:;<=>?@[\\]^_`{|}~ 
+abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!\"#$%&'()*+,-./:;<=>?@[\\]^_`{|}~ 
+abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!\"#$%&'()*+,-./:;<=>?@[\\]^_`{|}~ 
+abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!\"#$%&'()*+,-./:;<=>?@[\\]^_`{|}~ 
+abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!\"#$%&'()*+,-./:;<=>?@[\\]^_`{|}~ 
+abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!\"#$%&'()*+,-./:;<=>?@[\\]^_`{|}~ 
+abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!\"#$%&'()*+,-./:;<=>?@[\\]^_`{|}~ 
+abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!\"#$%&'()*+,-./:;<=>?@[\\]^_`{|}~ 
+abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!\"#$%&'()*+,-./:;<=>?@[\\]^_`{|}~ 
+abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!\"#$%&'()*+,-./:;<=>?@[\\]^_`{|}~ 
+abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!\"#$%&'()*+,-./:;<=>?@[\\]^_`{|}~ 
+abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!\"#$%&'()*+,-./:;<=>?@[\\]^_`{|}~ 
+abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!\"#$%&'()*+,-./:;<=>?@[\\]^_`{|}~ 
+abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!\"#$%&'()*+,-./:;<=>?@[\\]^_`{|}~ 
+abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!\"#$%&'()*+,-./:;<=>?@[\\]^_`{|}~ 
+abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!\"#$%&'()*+,-./:;<=>?@[\\]^_`{|}~ 
+abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!\"#$%&'()*+,-./:;<=>?@[\\]^_`{|}~ 
+abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!\"#$%&'()*+,-./:;<=>?@[\\]^_`{|}~ 
+abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!\"#$%&'()*+,-./:;<=>?@[\\]^_`{|}~
+abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!\"#$%&'()*+,-./:;<=>?@[\\]^_`{|}~ abcdefghijklmnopqrstuvwxyzABCD"
+#longest_substring($rly_long_str)
+
+class TestSimpleNumber < Test::Unit::TestCase
+  def test_simple
+    assert_equal(3, longest_substring('abcabcbb'))
+    assert_equal(1, longest_substring('bbbbb'))
+    assert_equal(3, longest_substring('pwwkew'))
+    assert_equal(9, longest_substring('rickdabosric'))
+    assert_equal(0, longest_substring(''))
+    assert_equal(1, longest_substring(' '))
+    assert_equal(3, longest_substring('dvdf'))
+    assert_equal(5, longest_substring('anviaj'))
+    assert_equal(6, longest_substring('asjrgapa'))
+    assert_equal(96, longest_substring($rly_long_str))
+  end 
+end
---
+++ amazon/two_sum.rb
@@ -0,0 +1,63 @@
+require "test/unit"
+
+# https://leetcode.com/explore/interview/card/amazon/76/array-and-strings/508/
+
+# Given an array of integers, return indices of the two numbers such that they add up to a specific target.
+
+# You may assume that each input would have exactly one solution, and you may not use the same element twice.
+
+# Example:
+
+# Given nums = [2, 7, 11, 15], target = 9,
+
+# Because nums[0] + nums[1] = 2 + 7 = 9,
+# return [0, 1].
+
+# @param {Integer[]} nums
+# @param {Integer} target
+# @return {Integer[]}
+
+
+# Brute force
+=begin
+def two_sum(nums, target)
+  nums.each_with_index do |n, i|
+    sub_array = nums[i+1..nums.length-1]
+    #p "#{n}|#{sub_array}"
+    sub_array.each_with_index do |m, j|
+      # p "#{n}|#{m}"
+      # p n+m
+      # p "#{i}|#{j+1}" if n+m == target
+      return [i,j+i+1] if n+m == target
+    end
+  end
+  return []
+end
+=end
+
+
+def two_sum(nums, target)
+  nums.each_with_index do |n, i|
+    complement = target - n
+    sub_array = nums.dup
+    sub_array.delete_at(i)
+    if (sub_array.include?(complement))
+      return [i,sub_array.index(complement)+1]
+    end
+  end
+end
+
+
+class TestSimpleNumber < Test::Unit::TestCase
+  def test_simple
+    assert_equal([0,1], two_sum([2, 7, 11, 15],9))
+    p '------'
+    assert_equal([0,2], two_sum([2, 7, 11, 15],13))
+    p '------'
+    assert_equal([0,3], two_sum([2, 7, 11, 15],17))
+    p '------'
+    assert_equal([1,2], two_sum([3, 2, 4],6))
+    p '------'
+    assert_equal([0,1], two_sum([3, 3],6))
+  end 
+end
